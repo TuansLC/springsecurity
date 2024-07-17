e7f02eaa71d163fc1758de6753ffc693ee0dc615
TuansLC <luucungtuan2907@gmail.com>
2024-07-17 23:54:55 +0700

    start code springsecsection1

---
+++ src/test/java/com/tuanlc/Springsecsection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanlc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsecsection1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
